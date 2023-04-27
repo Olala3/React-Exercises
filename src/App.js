84d115829d0871ae234ce1b70bebd77f92ec5f3b
Olala <olasabbouh@gmail.com>
2023-04-27 11:33:28 +0300

    exercise 1!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {Welcome} from './Exercises'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Welcome name='Ola'/>
         <a
           className="App-link"
           href="https://reactjs.org"
