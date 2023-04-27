1a9c801a96f7ebe085d51b14bedf6b68d36d1e29
Olala <olasabbouh@gmail.com>
2023-04-27 11:38:38 +0300

    exercise 5!

--- src/Exercises.js
+++ src/Exercises.js
@@ -4,7 +4,10 @@ export class Welcome extends React.Component {
 
     render(){
         return <div>
-            <p>Welcome {this.props.name}!</p>
+            { this.props.name
+               ? <p>Welcome {this.props.name}!</p>
+               : <p>Welcome Everyone!</p>
+            }
         </div>
     }
 }
