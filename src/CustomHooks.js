a62fcf08f85580bdf2d7e47683d1d2ed5eb70442
Olala <olasabbouh@gmail.com>
2023-05-12 13:51:04 +0300

    44 useCallback

--- src/CustomHooks.js
+++ src/CustomHooks.js
@@ -1,25 +1,25 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export function useCounter(initialValue=0){
     const [count, setCount] = useState(initialValue)
   
-    function handleIncrement() {
-        setCount(count =>  count + 1 )
-      };
+    const increment = useCallback(function handleIncrement() {
+      setCount(count =>  count + 1 )
+    }, [])
 
-    function handleDecrement() {
-        setCount(count =>  count - 1)
-    }
+    const decrement = useCallback(function handleDecrement() {
+      setCount(count =>  count - 1)
+    }, [])
 
-    function handleReset() {
-        setCount(initialValue)
-    }
+    const reset = useCallback(function handleReset() {
+      setCount(initialValue)
+    }, [])
 
     return {
         count: count,
-        increment: handleIncrement,
-        decrement: handleDecrement,
-        reset: handleReset
+        increment,
+        decrement,
+        reset
     }
 }
 
